66b6c58d8a57a28d01d2ba993fbce15c2ae8955c
alishagithubshamoun <alishashamoun12345@gmail.com>
2024-01-22 14:43:54 +0500

    notification add

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,14 @@
 import './bootstrap';
+
+// resources/js/app.js
+
+require('./bootstrap');
+
+window.toastr = require('toastr');
+
+// You can customize toastr options here if needed
+toastr.options = {
+    "closeButton": true,
+    "positionClass": "toast-top-right",
+    "progressBar": true,
+};
